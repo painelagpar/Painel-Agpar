9f075644aef67fa313d4c9b7317efa585ee19af6
painelagpar <painelagpar@gmail.com>
2026-09-06 17:17:10 -0300

    deploy: config.js (Painel do AGPAR)

---
+++ config.js
@@ -0,0 +1,5 @@
+// Painel do AGPAR - configuracao de conexao (gerada automaticamente)
+window.AGAR_CONFIG = {
+  SUPABASE_URL: "https://dilnvmrzdwikhmpuqgcg.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpbG52bXJ6ZHdpa2htcHVxZ2NnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0OTY3MjAsImV4cCI6MjEwNDA3MjcyMH0.TQHoZVU2_pGcT0WXV-Pwk8sVYnehEeJUpGenFyNZNPE"
+};
